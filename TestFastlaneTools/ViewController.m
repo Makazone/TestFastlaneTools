b0f0c9161811e6d9f8b4aa06cbaae95f299739b1
Stetsenko Makar <makazone@gmail.com>
2015-07-23 00:42:06 -0700

    Introduct ENV variables for Crashlytics

--- TestFastlaneTools/ViewController.m
+++ TestFastlaneTools/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Crashlytics/Crashlytics.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[Crashlytics sharedInstance] crash];
 }
 
 - (void)didReceiveMemoryWarning {
