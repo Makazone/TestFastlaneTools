4b60190a073c0a128948d7b670a7e14be1539de4
Stetsenko Makar <makazone@gmail.com>
2015-07-22 19:04:21 -0700

    Initial commit

---
+++ TestFastlaneTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFastlaneTools
+//
+//  Created by Makar Stetsenko on 22.07.15.
+//  Copyright (c) 2015 Makar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFastlaneTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFastlaneTools
+//
+//  Created by Makar Stetsenko on 22.07.15.
+//  Copyright (c) 2015 Makar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
